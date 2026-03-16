d6863ca23d03fe4844479609ad1574c267685361
nishanrokaya535-png <nishanrokaya535@gmail.com>
2026-03-16 09:09:15 +0545

    remove static export for vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */ 
-const nextConfig = { 
-  output: 'export', 
-  images: { unoptimized: true, domains: ['res.cloudinary.com'] }, 
-} 
-module.exports = nextConfig 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+}
+
